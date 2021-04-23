23ce7b82ae0d0c1e99591867eac1a421876a18ac
ThomasJohannessen <54627448+ThomasJohannessen@users.noreply.github.com>
2021-04-23 10:04:32 +0200

    Lagde Scraper exception klasse. IKKE implementert

---
+++ code/src/main/java/ScrapeException.java
@@ -0,0 +1,9 @@
+public class ScrapeException extends Exception {
+        public ScrapeException(String message) {
+            super(message);
+        }
+
+        public ScrapeException(){
+            super("The item you were looking for could not be found.");
+        }
+    }
