5c85ec454b09c810271124a132b58b5b203645a4
Jollokim <joakimjensenj@gmail.com>
2021-04-30 17:34:24 +0200

    commit before pull

--- code/src/main/java/Main.java
+++ code/src/main/java/Main.java
@@ -5,12 +5,12 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-//        Scraper sc = Scraper.buildScraperWithWebUrl("https://webscraper.io/test-sites/e-commerce/allinone");
+        Scraper sc = Scraper.buildScraperWithWebUrl("https://webscraper.io/test-sites/e-commerce/allinone/phones");
 
 //        Scraper sc = Scraper.buildScraperWithFile("DummyHTMLForScraper.html");
 
 
-        Scraper sc = Scraper.buildScraperWithString("<html lang=\"en\"><body><h1 id=\"header1\">Hello world</h1><p>This is my world</p></body></html>");
+//        Scraper sc = Scraper.buildScraperWithString("<html lang=\"en\"><body><h1 id=\"header1\">Hello world</h1><p>This is my world</p></body></html>");
 
         sc.printBeautyfull();
 
